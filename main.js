3979a7db0322c5a72d6892362e532ff8898a8992
Anri <anrihunanyan@gmail.com>
2021-06-11 17:47:30 +0400

    feat(*) corrected kitchen page

---
+++ main.js
@@ -0,0 +1,16 @@
+function sign() {
+  let sign_in = document.querySelector(".sign-in").style.display;
+  if (sign_in === "block") {
+    document.querySelector(".sign-in").style.display = "none";
+    document.querySelector(".sign-up").style.display = "block";
+  } else {
+    document.querySelector(".sign-in").style.display = "block";
+    document.querySelector(".sign-up").style.display = "none";
+  }
+}
+
+function navBar() {
+  document.querySelector(".header-nav").style.display === "block"
+    ? (document.querySelector(".header-nav").style.display = "none")
+    : (document.querySelector(".header-nav").style.display = "block");
+}
